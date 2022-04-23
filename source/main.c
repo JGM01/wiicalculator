508505e73f8959070fde9b746b64b0631d626225
JGM01 <jacobgermanamccray@gmail.com>
2022-04-23 00:40:43 -0400

    created initializeWii method

--- source/main.c
+++ source/main.c
@@ -1,3 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gccore.h>
+#include <wiiuse/wpad.h>
+
+void initializeWii();
+void prompt();
+int getOperand();
+bool getOperator();
+
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
 int main(int argc, char **argv) {
+    initializeWii();
+
+
+    
+
+
     return 0;
+}
+
+void initializeWii() {
+    VIDEO_Init();
+    WPAD_Init();
+
+    rmode = VIDEO_GetPreferredMode(NULL);
+    xfb = MEM_K0_TO_K1(SYS_AllocatorFramebuffer(rmode));
+
+    console_init(xfb, 20, 20, rmode -> fbWidth, rmode -> xfbHeight, rmode -> fbWidth * VI_DISPLAY_PIX_SZ);
+    VIDEO_Configure(rmode);
+    VIDEO_SetNextFramebuffer(xfb);
+
+    VIDEO_SetBlack(FALSE);
+    VIDEO_Flush();
+
+    VIDEO_WaitVSync();
+    if(rmode -> viTVMode&VI_NOW_INTERLACE) VIDEO_WaitVSync();
+    printf("\x1b[2;0H");
 }
